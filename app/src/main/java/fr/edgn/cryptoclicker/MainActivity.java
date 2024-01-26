b9085f8272108572d4b508b24a33e65bcb4a9aa9
Flemme <113991915+SteveTheSheep@users.noreply.github.com>
2024-01-26 11:47:18 +0100

    first commit

--- app/src/main/java/fr/edgn/cryptoclicker/MainActivity.java
+++ app/src/main/java/fr/edgn/cryptoclicker/MainActivity.java
@@ -2,7 +2,9 @@ package fr.edgn.cryptoclicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivityClicker.class);
+            startActivity(intent);
+        });
     }
 }
---
+++ app/src/main/java/fr/edgn/cryptoclicker/MainActivityClicker.java
@@ -0,0 +1,13 @@
+package fr.edgn.cryptoclicker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivityClicker extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_clicker);
+    }
+}
